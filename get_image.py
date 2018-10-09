73a4e1fc593c6ecda81cba65506428aba0c969ee
PeachFusionTea <victorxdang@gmail.com>
2018-10-08 20:45:06 -0500

    Add files via upload
    Added ability to resize image to desired size.

--- get_image.py
+++ get_image.py
@@ -1,3 +1,4 @@
+
 import os
 import csv
 import urllib.request
@@ -6,34 +7,39 @@ from PIL import Image
 # change constants as needed
 IMAGE_URL_FOLDER = "ImageURLs/" # this folder will hold the files that has the URLs
 IMAGE_DUMP_FOLDER = "ImageDump/" # this folder is where the pictures will be saved to
+IMAGE_SIZE = (500,500) # final size of the images
 
 
-def get_image(url, image_path):
+# Added image resizing in this function
+def get_image(url, image_path, size = IMAGE_SIZE):
     urllib.request.urlretrieve(url, image_path)
+    image = Image.open(image_path)
+    image = image.resize(IMAGE_SIZE)
+    image.save(image_path)
 
 
+# seems pointless for now
 def read_txt(file_location, filename):
     with open(file_location + filename) as txt_file:
         i = 0
-        image_name = os.path.splitext(filename)[0] # split the filename from the extension
 
         for line in txt_file:
             if "https://" in line:
                 # grabs only the URL from the line in the text file and removes the leading whitespaces
                 url = line.split(" : ")[0].replace("\"", "").lstrip()
-                get_image(url, IMAGE_DUMP_FOLDER + image_name + "_" + str(i) + ".jpg")
+                get_image(url, IMAGE_DUMP_FOLDER + str(i) + ".jpg")
                 i += 1
 
 
+# grab only the links from the csv files
 def read_csv(file_location, filename):
     with open(file_location + filename) as csv_file:
         i = 0
-        image_name = os.path.splitext(filename)[0] # split the filename from the extension
         reader = csv.reader(csv_file, delimiter = ",")
 
         for line in reader:
             if "https://" in line[0]:
-                get_image(line[0], IMAGE_DUMP_FOLDER + image_name + "_" + str(i) + ".jpg")
+                get_image(line[0], IMAGE_DUMP_FOLDER + str(i) + ".jpg")
                 i += 1
 
 
