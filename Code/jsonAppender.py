9ccf1f4899faa802e3b085cc0309c84d98741795
Jose Ruiz <joseruiz75211@hotmail.com>
2018-10-24 22:46:22 -0500

    fixxed my annotations and added json to jsonAppender

--- Code/jsonAppender.py
+++ Code/jsonAppender.py
@@ -1,4 +1,5 @@
 import os
+import json
 from os import listdir
 from os.path import isfile, join
 
@@ -23,12 +24,35 @@ def cat_json(output_filename, input_filenames):
 def mangle(s):
     return s.strip()[1:-1] #start:end possibly need to change
 
+def combine_annotations_in_dir(directory):
+	images_w_annotations = {}
+	for file in os.listdir(directory):
+		with open(os.path.join(directory, file), 'r') as f:
+			line = f.readline()	#we expect the json files to be a single long line
+
+		parsed_json = json.loads(line)
+
+		for k, v in parsed_json.items():
+			if k == '_via_settings' or k == 'region':
+				print('sorry file: {0}, is not the right type make sure you export annotations as json'.format(file))
+				break
+			
+			#regions is where the annotations are stored if empty file has no annotations
+			if v['regions'] != []:
+				images_w_annotations[k] = v
+
+	return images_w_annotations
+
 def main():
-    #create new file name 'annotations_group.json' to append the file data
-    output_file = open('annotations_group.json', 'w+')
-    mypath = os.getcwd()
-    input_files = [f for f in os.listdir(mypath) if isfile(join(mypath, f))]
-    cat_json('annotations_group.json', input_files)
+	mypath = os.getcwd()
+	#input_files = [f for f in os.listdir(mypath) if isfile(join(mypath, f))]
+	path_to_json = os.path.join('..', 'annotations')
+	annotations = combine_annotations_in_dir(path_to_json)
+	print(annotations)	
+
+		#images_w_annotations.sort()
+	print(annotations)
+
 
 if __name__ == '__main__':
     main()
