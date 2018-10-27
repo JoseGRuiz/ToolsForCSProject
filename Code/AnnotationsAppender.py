d75e2e8efc7633dddb8ef6800034b4a647506308
Jose Ruiz <joseruiz75211@hotmail.com>
2018-10-27 00:03:50 -0500

    AnnotationAppender now works with csv files

--- Code/AnnotationsAppender.py
+++ Code/AnnotationsAppender.py
@@ -8,7 +8,6 @@ OUTPUT_CSV_FILENAME = 'annotations_group.csv'
 
 '''
 combines the json files in a given directory
-for now assuming the directory contains only json files
 returns a dictionary with all annotations merged 
 '''
 def combine_json_in_dir(directory):
@@ -36,31 +35,32 @@ def combine_json_in_dir(directory):
 
 	return images_w_annotations
 
+'''
+combines csv files in a given directory
+returns a header and a list with all annotations merged
+'''
 def combine_csv_in_dir(directory):
-	images_w_annotations = {}
+	header = None
+	images_w_annotations = []
 	for file in os.listdir(directory):
 		if file == OUTPUT_CSV_FILENAME: #skip the merged file
 			continue
-		if 'csv' not in file: #skip non csv files
+		elif 'csv' not in file: #skip non csv files
 			print('{0} is not a csv file skipping'.format(file))
 			continue
 
-		with open(os.path.join(directory, file), 'r') as f:
-			line = f.readline()	#we expect the csv files to be a single long line
+		with open(os.path.join(directory, file), 'r') as csvfile:
+			csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+			#header is the first line it contains the title of the 
+			#matching item in the list ex. filename column contains file names
+			header = csvreader.__next__()
+			rc_index = header.index('region_count')
+			for row in csvreader:
+				#we want to copy annotations of pictures with at least 1 annotation
+				if int(row[rc_index]) > 0: 
+					images_w_annotations.append(row)
 
-		print(line)
-		'''parsed_json = json.loads(line)
-
-		for k, v in parsed_json.items():
-			if k == '_via_settings' or k == 'region':
-				print('sorry file: {0}, is not the right type make sure you export annotations as json'.format(file))
-				break
-			
-			#regions is where the annotations are stored if empty file has no annotations
-			if v['regions'] != []:
-				images_w_annotations[k] = v
-
-	return images_w_annotations'''
+	return header, images_w_annotations
 
 def main():
 	path_to_files = os.path.join('..', 'annotations')
@@ -75,10 +75,16 @@ def main():
 		with open(os.path.join(path_to_files, OUTPUT_JSON_FILENAME), 'w') as f:
 			f.write(annotations_as_json)
 	elif sys.argv[1].lower() == 'csv':
-		annotations = combine_csv_in_dir(path_to_files)
+		header, annotations = combine_csv_in_dir(path_to_files)
 		print(annotations)
-		#with open(os.path.join(path_to_files, OUTPUT_CSV_FILENAME), 'wb') as f:
-			#csv_writter = csv.writer(f, delimiter=',', quotechar = '|', quoting=csv.QUOTE_MINIMAL)
+
+		with open(os.path.join(path_to_files, OUTPUT_CSV_FILENAME), 'w') as f:
+			csv_writter = csv.writer(f, delimiter=',',
+											quotechar = '|', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+			
+			csv_writter.writerow(header)
+			for row in annotations:
+				csv_writter.writerow(row)
 
 
 if __name__ == '__main__':
