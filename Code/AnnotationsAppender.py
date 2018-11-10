088291c218c797e6d5c429757f3992aa4af8aca7
Jose Ruiz <joseruiz75211@hotmail.com>
2018-11-10 16:04:54 -0600

    added more comments to AnnotationsAppender, and ImagePartitioner

--- Code/AnnotationsAppender.py
+++ Code/AnnotationsAppender.py
@@ -3,14 +3,18 @@ import sys
 import json
 import csv
 
+#globals for names to use for the merged files
 OUTPUT_JSON_FILENAME = 'annotations_group.json'
 OUTPUT_CSV_FILENAME = 'annotations_group.csv'
 
-'''
-combines the json files in a given directory
-returns a dictionary with all annotations merged 
-'''
 def combine_json_in_dir(directory):
+	"""
+	combines the json files in a given directory
+	returns a dictionary with all annotations merged
+
+	Args:
+	directory: the directory containing the files to merge
+	"""
 	images_w_annotations = {}
 	for file in os.listdir(directory):
 		if file == OUTPUT_JSON_FILENAME: #skip the merged file
@@ -22,24 +26,32 @@ def combine_json_in_dir(directory):
 		with open(os.path.join(directory, file), 'r') as f:
 			line = f.readline()	#we expect the json files to be a single long line
 
+		#interpret the input as json 
 		parsed_json = json.loads(line)
 
+		#make sure the right type of file is used there's a few types
+		#of json that can come out of via but only 1 is the right type
 		for k, v in parsed_json.items():
 			if k == '_via_settings' or k == 'region':
 				print('sorry file: {0}, is not the right type make sure you export annotations as json'.format(file))
 				break
 			
-			#regions is where the annotations are stored if empty file has no annotations
+			#regions is where the annotations are stored, if empty file has no annotations
+			#@TODO: right now images with no tags simply ignored, might not be the best policy
+			#becasue it can be beneficial to keep the empty images in the data 
 			if v['regions'] != []:
 				images_w_annotations[k] = v
 
 	return images_w_annotations
 
-'''
-combines csv files in a given directory
-returns a header and a list with all annotations merged
-'''
 def combine_csv_in_dir(directory):
+	"""
+	combines csv files in a given directory
+	returns a header and a list with all annotations merged
+
+	Args:
+	directory: the directory containing the files to merge
+	"""
 	header = None
 	images_w_annotations = []
 	contributions = []
@@ -50,11 +62,16 @@ def combine_csv_in_dir(directory):
 			print('{0} is not a csv file skipping'.format(file))
 			continue
 
+		#open the file as a csv file and copy the row 
 		with open(os.path.join(directory, file), 'r') as csvfile:
 			csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
 			#header is the first line it contains the title of the 
 			#matching item in the list ex. filename column contains file names
 			header = csvreader.__next__()
+
+			#find the index of these desired properties
+			#region count is how many regions of the image are annotated(how many boxes)
+			#region_shape_attributes is which type of annotation style was used (box, polygon, etc...)
 			rc_index = header.index('region_count')
 			region_shape_attr = header.index('region_shape_attributes')
 			file_contributions = {}
@@ -62,29 +79,36 @@ def combine_csv_in_dir(directory):
 				#we want to copy annotations of pictures with at least 1 annotation
 				#also only accept rect type annotations for now
 				if int(row[rc_index]) > 0 and 'rect' in row[region_shape_attr]:
+					#keep track of which file contributed which image and how many 
+					#individual annotations the made and how many overall images they did
 					if row[0] in file_contributions:
 						file_contributions[row[0]] += 1
 					else:
 						file_contributions[row[0]] = 1
+					#add the desired row to the combined list
 					images_w_annotations.append(row)
+			#append the file's contributions to the list of all contributions
 			contributions.append((file, file_contributions))
 
 	return header, images_w_annotations, contributions
 
-'''
-this format is preffered by Ayesha who is writing
-the main program, it is a modified subset of the via output
-the basic form is file, x, y, x+w, y+h, class
-'''
+
 def extract_useful(header, csv_annotations):
+	"""
+	this format is preffered by keras-retinanet
+	the main program, it is a modified subset of the via output
+	the basic form is file, x, y, x+w, y+h, class
+	"""
+	#locate the indexes of the data from the header
 	filename_index = header.index('filename')
 	region_shape_attr = header.index('region_shape_attributes')
 	#region shape attributes are split up into 5 strings
+	#the fifth one is the region attribute we want
 	region_attr = region_shape_attr + 5
 
 	new_annotations = []
 	for row in csv_annotations:
-		#instead of hardcoding this make it an input param or at least a global
+		#@refactor:instead of hardcoding this make it an input param or at least a global
 		new_row = [os.path.join('/home/ayeshag/ImageDump/', row[filename_index])]
 		#first 2 are copies of x and y 
 		for i in range(1, 3):
@@ -107,6 +131,7 @@ def extract_useful(header, csv_annotations):
 
 		new_annotations.append(new_row)
 
+	#this header describes the column it is above
 	new_header = ['filename', 'TopL_x', 'TopL_y', 'BottomR_x', 'BottomR_y', 'class']
 	return new_header, new_annotations
 
@@ -118,6 +143,7 @@ def printContributions(contributions):
 	for file, file_contributions in contributions:
 			print('file {0} contributed {1} annotated pictures to the merged file'.format(
 				file, len(file_contributions)))
+			#sum up the individual annotations contriubted by each person
 			individual_annotations = 0
 			for annottation in file_contributions:
 				individual_annotations += file_contributions[annottation]
@@ -126,36 +152,49 @@ def printContributions(contributions):
 				individual_annotations / len(file_contributions)))
 
 def main():
+	"""
+	this program combines multiple files of annotations
+	generated by via, and it also prints out a report of 
+	how the files contributed to the merged file
+	via outputs 2 types of annotation files json and csv
+	"""
+
+	#make sure all necessary arguments are passed into the program
+	#need a path to where the files are, and which type of file csv or json 
 	if len(sys.argv) < 3:
 		print('error: make sure to call in this form python <path_to_files> <type_of_fies>')
 		exit(1)
 
 	path_to_files = sys.argv[1]
 
-	if sys.argv[2].lower() == 'json':
+	#@TODO: the json version of this program is out of date compared to the csv version
+	if sys.argv[2].lower() == 'json':#merge json files
 		annotations = combine_json_in_dir(path_to_files)
-		#print(annotations)
 
 		annotations_as_json = json.dumps(annotations)
-		#right now I think we're just rewriting the entire file each time 
+		#@refactor: right now I think we're just rewriting the entire file each time 
 		#would it be worth it to make it only append? 
 		with open(os.path.join(path_to_files, OUTPUT_JSON_FILENAME), 'w') as f:
 			f.write(annotations_as_json)
 
 		print('succesfully joined json files')
-	elif sys.argv[2].lower() == 'csv':
+	elif sys.argv[2].lower() == 'csv':#merge csv files 
 		header, annotations, contributions = combine_csv_in_dir(path_to_files)
-		#print(annotations)
 
+		#change the form of the data to the one perfered by keras
 		header, annotations = extract_useful(header, annotations)
+		#write out to the file as a csv 
 		with open(os.path.join(path_to_files, OUTPUT_CSV_FILENAME), 'w') as outputcsv:
+			#create the csv writter object 
 			csv_writter = csv.writer(outputcsv, delimiter=',',
 											quotechar = '|', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
 			
+			#write one row at a time first row is the header
 			csv_writter.writerow(header)
 			for row in annotations:
 				csv_writter.writerow(row)
 
+		#print success and output statistics 
 		print('succesfully joined csv files')
 		printContributions(contributions)
 
--- Code/ImagePartitioner.py
+++ Code/ImagePartitioner.py
@@ -4,62 +4,97 @@ import csv
 from random import shuffle
 from zipfile import ZipFile
 
-'''
-splits files up between people, specifically those in our 
-group first it was link distribution now it is image file 
-distribution and compression to zip files
-'''
+
 def main():
+	"""
+	splits files up between people, specifically those in our 
+	group first it was link distribution now it is image files 
+	distribution and compression to zip files
+	"""
+	#list of students involved in the project
+	#@TODO: make this be read from a file or somehow more dynamic 
 	students = ['Logan Padon', 'Ayan Paul', 'Ayesha Gurnani',
 						'Divyesh Patel', 'Jose Ruiz', 'Kenny Hoang', 'Rutuja Kaushike', 'Victor Dang']
+	#give a zip file containing all Images to the program
+	#this assumes that the script and zip file are in the same directory
+	#@TODO: this could also be an argument to the program
 	file_name = 'ImageDump.zip'
+
+	#force_dump is a program argument that if passed in as true
+	#will force the zipfile to be read, prevents unnecessary unzipping
 	if len(sys.argv) > 1:
 		force_dump = bool(sys.argv[1])
 	else: 
 		force_dump = False
 
+	picture_names = os.listdir('ImageDump')
 	with ZipFile(file_name, 'r') as zip:
-		picture_names = os.listdir('ImageDump')
+		#only exctract from  the zip file if not all pictures are yet in 
+		#the directory or if forced to 
 		if force_dump or len(picture_names) != len(zip.namelist()):
 			zip.extractall()
 
-	picture_names = os.listdir('ImageDump')
-
-	#how many should each student worry about 
+	#divide the number of pictures by the ammount of students
+	#and calculate how many each student should be responsible for
 	per_student = len(picture_names)/len(students)
-	per_student = int(per_student)	#lazy floor
+	per_student = int(per_student)	#@refactor: lazy floor
 
-	#add some randomness
+	#add some randomness to reduce probability that someone gets only 
+	#bad images or someone gets an unfair ammount of work 
 	shuffle(picture_names)
 
 	#create a dictionary with student names as keys 
-	#and slices of the URL list as the URLs they will work with
+	#and slices of the image list as the images they will work with
 	start = 0
 	end = per_student 
 	responsibilities = {}
+	#give each student the same amount of work, next slice starts 
+	#where the last slice ends 
 	for student in students:
+		#take a slice of the total list as the student's list
 		responsibilities[student] = picture_names[start:end]
+		#update start and end to mark the next slice
 		start = end
 		end += per_student
 
+	#make the directory if it doesn't exit
 	os.makedirs('Images_compressed', exist_ok = True)
-	#create and write the assignmets for each student
+	#for the student's list of responsibilities zip their assignments
+	#into a zip file file is named the same as the student
 	for k in responsibilities:
 		write_files_to_zip(os.path.join('Images_Compressed', k + '.zip'),
 										'ImageDump', responsibilities[k])
 
 	print('files zipped succesfully!')
 
-#read in links for a given file
 def read_links(file):
+	"""
+	read in urls for a given csv file
+	this function no longer serves a purpose in the current
+	implementation but it might be usefull later
+
+	Args:
+	file: the file to read the links from
+	"""
+
+	#open the file as a csv
 	with open(file) as csv_file:
 		reader = csv.reader(csv_file, delimiter = ",")
 
+		#assume that the urls are the first value in the csv
 		links = [t[0] for t in reader if "https://" in t[0]]
+	
 	return links
 
-#write files to a zip file, given a path and a list of files
 def write_files_to_zip(path_to_write, path_to_read, files):
+	"""
+	writes files to a zip file
+
+	Args: 
+	path _to_write: where to save the file to 
+	path_to_read: the path were the files to be zipped are
+	files: a list of files 
+	"""
 	with ZipFile(path_to_write, 'w') as zip:
 		for file in files:
 			zip.write(os.path.join(path_to_read, file))
